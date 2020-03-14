da9f2c57dd446a08b7c62db0df228eb3f8401afd
l3lackheart <dgbao1340@gmail.com>
2020-03-14 19:38:31 +0700

    feat: setup router for event list

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link :to="{ name: 'Home' }">Home</router-link> |
-      <router-link :to="{ name: 'About' }">About</router-link>
+      <router-link :to="{ name: 'event-list' }">List</router-link> |
+      <router-link :to="{ name: 'event-show', params: { eventId: 1 } }"
+        >Event #1</router-link
+      >
+      |
+      <router-link :to="{ name: 'event-create' }">Create Event</router-link>
     </div>
     <router-view />
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import EventList from '../views/EventList.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'event-list',
+    component: EventList
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/:eventId',
+    name: 'event-show',
+    component: () => import('../views/EventShow.vue')
   },
   {
-    path: '/about-us',
-    redirect: { name: 'About' }
+    path: '/create',
+    name: 'event-create',
+    component: () => import('../views/EventCreate.vue')
   }
 ]
 
